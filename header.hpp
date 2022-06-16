d8d10c20e88d4ea4aec8bc5599982f573bfca936
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-16 18:43:15 +0100

    header cpp

---
+++ header.hpp
@@ -0,0 +1,14 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+
+#include <iostream>
+using namespace std;
+
+
+int ft_strlen(std::string str);
+void reverse(std::string str);
+void cap(std::string str);
+int is_prime(int nb);
+int ft_atoi(std::string str);
+
+#endif
